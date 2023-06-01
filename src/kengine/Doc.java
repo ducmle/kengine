d9b0b31568dccf96f19c8409aac87e5981595e2e
Duc Minh Le <lemduc@gmail.com>
2023-06-01 11:25:26 +0700

    version 1.0

--- src/kengine/Doc.java
+++ src/kengine/Doc.java
@@ -11,7 +11,7 @@ import utils.NotPossibleException;
  * @overview A document contains a title and a text body
  * 
  * @see "Program Development in Java", pgs: 314,322,333
- * @version 1.0, 3.0
+ * @version 1.0
  * @author dmle
  * 
  */
@@ -19,8 +19,6 @@ public class Doc {
   private String d; // the document content
   private String title; // the document title
   private String body; // the document body
-
-  private Vector docWords; // the sequence of document words
 
   /**
    * Constructor method
@@ -108,161 +106,11 @@ public class Doc {
    * @version 3.0: added this method
    */
   public Iterator words() {
-    // extracts body
-    body();
-
-    if (body == null)
-      return null;
-    
-    // scans the body to return only words (without tags)
-    // assumes that Doc is immutable
-    // note: recall that body has the enclosing <body></body> tag pairs
-    if (docWords == null) {
-      docWords = new Vector();
-      final char OPEN_TAG = '<';
-      final char CLOSED_TAG = '>';
-      // final char FORWARD_SLASH = '/';
-      final char NEW_LINE = '\n';
-      final String[] SPECIALS = { NEW_LINE+"", "\t" };
-      final boolean SKIP_SPECIAL_HTML = true;
-      //final String[] JUNK_CHARS = { "`","``","''","(",")","-","[","]",":",";","\"",".",",","...","<",">"}; 
-                 
-      // reads the content one character at a time, stop each time
-      // we get to a stop-word (e.g. space or a tag)
-      char[] chars = body.toCharArray();
-      char c;
-      String w = null;
-      String reading = null;
-      boolean skip = false;
-      for (int i = 0; i < chars.length; i++) {
-
-        c = chars[i];
-
-        // if we have found a new open tag
-        if (!skip && c == OPEN_TAG) {
-          // skip to locate the next closing and stop chars
-          skip = true;
-          reading = "";
-          continue;
-        }
-
-        if (skip) {
-          reading += c;
-          // check closing and stop tags
-          if (c == CLOSED_TAG) { // end of opening tag, beginning text
-            // if told to skip special tags and reading
-            // so far shows that it is some special tag then do nothing
-            if (SKIP_SPECIAL_HTML) {
-              // skip <script> and <style>
-              if (!(reading.startsWith("script") || 
-                  reading.startsWith("SCRIPT") ||
-                  reading.startsWith("style") || 
-                  reading.startsWith("STYLE")
-                  )) {
-                w = "";
-              }
-            } else {
-              w = "";
-            }
-          } else if (c == OPEN_TAG) { // begin a new tag, end of text
-            // process reading
-            if (w != null) {
-              w.trim();
-              for (int j = 0; j < SPECIALS.length; j++) {
-                if (w.startsWith(SPECIALS[j]))
-                  w = w.substring(1); // remove special char at the beginning
-                if (w.endsWith(SPECIALS[j]))
-                  w = w.substring(0, w.length() - 1);// remove special char at
-                                                     // the end
-                
-                // replace all remaining special chars by spaces
-                w = w.replaceAll(SPECIALS[j], " ");
-              }
-
-              if (!w.equals("")) {
-                String[] witems = w.split(" ");
-                for (int j = 0; j < witems.length; j++) {
-                  String witem = witems[j].trim();
-                  if (!witem.equals("")) {
-                    //for (int x = 0; x < JUNK_CHARS.length; x++) 
-                    //  witem = witem.replaceAll(JUNK_CHARS[x], "");
-                    
-                    docWords.add(witem);
-                  }
-                }
-              }
-              // reset word
-              w = null;
-            }
-            reading = null;
-            // continue to parse from c
-            skip = false; // stop skipping
-            // decrement i so that we start from c from the next iteration
-            i--;
-          } else if (w != null) {
-            // still inside a text
-            // update word
-            if (c != NEW_LINE)
-              w += c;
-          }
-          // continue; // skip
-        }
-      }
-    } // end body processing
-
-    // create a generator from the body words
-    // note: we could have used the built-in Vector.iterator() method here
-    // instead.
-    if (docWords != null)
-      return new WordGenerator(docWords);
-    else
-      return null;
+    // stub
+    return null;
   }
 
   public String toString() {
     return title();
-  }
-  
-  /**
-   * @overview A generator implementation that is used by <code>Doc.words()</code> to 
-   *           return a word iterator of a document.
-   * 
-   * @author dmle
-   *
-   */
-  class WordGenerator implements Iterator {
-    Vector words;
-    int currWordIndex = -1;
-
-    /**
-     * 
-     * @param words
-     *          a word vector
-     * @requires <code>words</code> must not be <code>null</code>
-     */
-    WordGenerator(Vector words) {
-      this.words = words;
-    }
-
-    public boolean hasNext() {
-      return (currWordIndex < words.size() - 1);
-    }
-
-    public Object next() {
-      currWordIndex++;
-      return words.get(currWordIndex);
-    }
-
-    /**
-     * @effects throws
-     *          <code>NotImplementedException<code> because <code>Doc</code> is
-     *          immutable
-     * 
-     */
-    public void remove() throws NotImplementedException {
-      // not implemented
-      throw new NotImplementedException(
-          "WordGenerator.remove: Doc is immutable");
-    }
   }
 }
